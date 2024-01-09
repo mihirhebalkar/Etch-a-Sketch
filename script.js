309073acd5dc16e4cd0f745c36f8204ad4ea16d3
mihirhebalkar <mihirhebalkar07@gmail.com>
2024-01-09 13:13:02 +0530

    Added  grid using flexbox and prompt.

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function setGrid(container){
     let num=prompt("Enter number of grids(less than 100): ");
     for(let i=0;i<num*num;i++)
     {
-        const newDiv=document.createElement("div");
-        newDiv.style.height=`400/${num}`
+        let newDiv = document.createElement("div");
+        newDiv.style.height=`calc(600px/${num})`;
+        newDiv.style.width=`calc(600px/${num})`;
+        newDiv.style.backgroundColor = 'red';
         container.append(newDiv);
     }
 }
