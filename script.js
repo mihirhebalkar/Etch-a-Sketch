c2f79becaaec9d7d5ffb096ebc72be7f98cc7028
mihirhebalkar <mihirhebalkar07@gmail.com>
2024-01-09 23:36:33 +0530

    Created functions for erase, draw and new grid buttons.

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-
 const container=document.querySelector('.container');
 
+
 function setGrid(container){
+    container.innerHTML='';
     let num=prompt("Enter number of grids(less than 100): ");
-    for(let i=0;i<num*num;i++)
+    if(num>=100 || num<10)
     {
-        let newDiv = document.createElement("div");
-        newDiv.style.height=`calc(600px/${num})`;
-        newDiv.style.width=`calc(600px/${num})`;
-        newDiv.style.backgroundColor = 'black';
-        container.append(newDiv);
+        alert("Enter a value more than 10 and less than 100");
+        setGrid(container);
     }
+    else{
+        for(let i=0;i<num*num;i++)
+        {
+            let newDiv = document.createElement("div");
+            newDiv.style.height=`calc(600px/${num})`;
+            newDiv.style.width=`calc(600px/${num})`;
+            newDiv.style.backgroundColor = 'aquamarine';
+            container.append(newDiv);
+        }
+        
+    }
+    hover('black');
 }
 
-setGrid(container)
-
-const clrdiv=document.querySelectorAll('.container div');
-
-clrdiv.forEach(div => {
+function hover(color){
+    const clrdiv=document.querySelectorAll('.container div');
+    clrdiv.forEach(div => {
     div.addEventListener('mouseover',()=>{
-    div.style.backgroundColor='white';
+    div.style.backgroundColor=color;
     });
 });
+}
+const newgridBtn=document.querySelector('#new');
+newgridBtn.addEventListener('click',()=>{
+    setGrid(container);
+})
+
+const eraseBtn=document.querySelector('#eraser');
+eraseBtn.addEventListener('click',()=>{
+    hover('aquamarine')
+})
+
+const penBtn=document.querySelector('#pen');
+penBtn.addEventListener('click',()=>{
+    hover('black')
+})
+
+
+setGrid(container);
+
